85b46c66003c30ecb6d21bd1fdb95dda7ec7099e
philastrio <han_gang@naver.com>
2019-01-03 06:12:57 +0900

    Ready

--- app.js
+++ app.js
@@ -3,11 +3,8 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
 const app = express();
-const PORT = 4000;
 
-const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
 const handleHome = (req, res) => res.send("Hello from my House");
 const handleProfile = (req, res)  => res.send("You are on my profile");
 
@@ -21,4 +18,4 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+export default app;
